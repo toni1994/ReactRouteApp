06a820b5316ed60e057280f8de734c0df03d8221
Toni <tonibuzov2011@gmail.com>
2017-09-29 15:39:33 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={style.wrapper}>
+          Hello !!!
+      </div>
+    );
+  }
+}
+
+export default App;
