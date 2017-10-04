5b46c6ee3cbffbe4ca827a265e2c8638a8aa5a41
Toni <tonibuzov2011@gmail.com>
2017-10-05 00:50:54 +0200

    design1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 import style from './App.scss';
 
+const imgLocal = require('./images/helius_logo.png');
+
 class App extends Component {
+ 
   render() {
     return (
       <div className={style.wrapper}>
         <div className={style.navigation}> 
             <div className={style.navigationHeader}> 
+              <img className={style.logo} src={imgLocal} alt="Helius logo"/>
+              <div className={style.title}>{'helius'}</div>
+            </div>
+            <div className={style.navigationWrapper}>
+                <div className={style.navigationContainer}> Prototyper </div>
+                <div className={style.navigationContainer}> Property Arhitect </div>    
             </div>
         </div>
+
         <div className={style.module}>
 
-        <div className={style.topBar}>
-          <div className={style.topBarWrapper}> 
-            <div className={style.modulName}> Area prototyper</div>
-            <div className={style.topBarIcon}> bell</div>
-            <div className={style.topBarIcon}> notti</div>
-            <div className={style.topBarProfile}> Helius profico</div>
+          <div className={style.topBar}>
+            <div className={style.topBarWrapper}> 
+              <div className={style.modulName}> Area prototyper</div>
+              <div className={style.topBarIcon}> bell</div>
+              <div className={style.topBarIcon}> notti</div>
+              <div className={style.topBarProfile}> Helius profico</div>
+            </div>
           </div>
-         </div>
 
         <div className={style.moduleContainer}> 
         <div className={style.moduleHeader}> </div>
