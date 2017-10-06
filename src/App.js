a600f7dc76975107037f5e3f836e31b878f080b9
Toni <tonibuzov2011@gmail.com>
2017-10-06 14:43:06 +0200

    design3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import style from './App.scss';
-import Dotdotdot from 'react-dotdotdot';
-
+import Search from 'react-icons/lib/fa/search'
+import Bell from 'react-icons/lib/fa/bell'
+import Folder from 'react-icons/lib/fa/folder'
+import FaFolderOpenO from 'react-icons/lib/fa/folder-open-o'
+import FaClose from 'react-icons/lib/fa/close'
+import FaCaretDown from 'react-icons/lib/fa/caret-down'
 const imgLocal = require('./images/helius_logo.png');
 
 class App extends Component {
@@ -16,7 +20,7 @@ class App extends Component {
             </div>
             <div className={style.navigationWrapper}>
                 <div className={style.navigationContainer}> Prototyper </div>
-                <div className={style.navigationContainer}> Property Arhitect </div>    
+                <div className={style.navigationContainer}> Property Architect </div>    
             </div>
         </div>
 
@@ -25,22 +29,31 @@ class App extends Component {
           <div className={style.topBar}>
             <div className={style.topBarWrapper}> 
               <div className={style.modulName}> Area prototyper</div>
-              <div className={style.topBarIcon}> bell</div>
-              <div className={style.topBarIcon}> notti</div>
-              <div className={style.topBarProfile}> Helius profico</div>
+              <div className={style.topBarIcon}> <Bell /> </div>
+              <div className={style.topBarIcon}> <Folder /></div>
+              <div className={style.topBarProfile}> <div className={style.imageProfile}> <img src="http://api.helius.proficodev.com/public/uploads/user/56a0e753aaf71500641642742/56a0e753aaf71500641642742.png" alt="profileImage" > 
+              </img> </div> <div className={style.profilName}> Helius Profico </div> <FaCaretDown/> </div>
             </div>
           </div>
 
         <div className={style.moduleContainer}> 
-        <div className={style.moduleHeader}> </div>
+        <div className={style.moduleHeader}>
+          <div className={style.multiSearchContainer}>
+            <div className={style.search}> <div className={style.searchIcon}> <Search /> </div> <input placeholder="Search folders by name"/> <div className={style.closeIcon}> <FaClose /> </div> </div>
+            <div className={style.searchSection}> <span> SORT BY  </span> Name / ASC </div>
+            <div className={style.searchSection}> <span> FILTERS  </span> 0 active </div>
+            <div className={style.buttonContainer}>  <FaCaretDown/> </div>
+          </div>
+          
+        </div>
 
-       
         <div className={style.moduleBody}> 
           <div className={style.grid}>
 
             <div className={style.moduleBodyContainer}>   
               <div className={style.moduleBodyContent}>
-                  <div className={style.folder}> 
+                  <div className={style.folder}>
+                      <FaFolderOpenO /> 
                       <div className={style.folderText}> 
                         <div className={style.folderName}> Bar </div> 
                         <div className={style.numberOfItems}> 6 items </div>
@@ -52,8 +65,9 @@ class App extends Component {
 
             <div className={style.moduleBodyContainer}>   
               <div className={style.moduleBodyContent}>
-                  <div className={style.folder}> 
-                      <div className={style.folderText}> 
+                  <div className={style.folder}>
+                       <FaFolderOpenO /> 
+                      <div className={style.folderText}>
                         <div className={style.folderName}> Bar </div> 
                         <div className={style.numberOfItems}> 6 items </div>
                       </div> 
