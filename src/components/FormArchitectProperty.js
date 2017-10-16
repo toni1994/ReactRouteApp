e37d2a3f8874c636b2a3030729214f7b57b2387a
Toni <tonibuzov2011@gmail.com>
2017-10-16 17:00:23 +0200

    formAdd

--- src/components/FormArchitectProperty.js
+++ src/components/FormArchitectProperty.js
@@ -5,6 +5,40 @@ import FormButton from './FormButton';
 
 import style from '../styles/Form.scss';
 
+const validate = values => {
+    const errors = {};
+    if (!values.propertyName) {
+      errors.propertyName = 'Required';
+    }
+    if (!values.yearOpened) {
+      errors.yearOpened = 'Required';
+    }
+    if (!values.address) {
+        errors.address = 'Required';
+      }
+      if (!values.phone) {
+        errors.phone = 'Required';
+      }
+    return errors;
+};
+
+const renderFormField = ({
+    input,
+    label,
+    type,
+    meta: { touched, error }
+  }) => (
+    <div className={style.inputContainer}>
+      <div className={style.label}>  <label >{label}</label> </div>
+      <div className={style.input}>
+        <input {...input} placeholder={label} type={type} />
+      </div>
+      <div className={style.error}>  {touched &&
+          ((error && <span>{error}</span>) )} </div>
+    </div>
+  )
+
+
 const FormPropArchitectProperty = (props) => (
     <form className={style.form}>
         <div className={style.header}>
@@ -12,6 +46,10 @@ const FormPropArchitectProperty = (props) => (
             <p> Add Property </p>
         </div>
         <div className={style.body}>
+                <Field name="propertyName" component={renderFormField} type="text" label="Property name" />
+                <Field name="yearOpened" component={renderFormField} type="text" label="Year opened" />
+                <Field name="address" component={renderFormField} type="text" label="Address" />
+                <Field name="phone" component={renderFormField} type="text" label="Phone" />
         </div>
         <div className={style.footer}>
             <FormButton notPrimary> CANCEL
@@ -22,4 +60,7 @@ const FormPropArchitectProperty = (props) => (
     </form>
 )
 
-export default FormPropArchitectProperty;
+export default reduxForm({
+    form: 'addProperty',
+    validate,
+})(FormPropArchitectProperty)
